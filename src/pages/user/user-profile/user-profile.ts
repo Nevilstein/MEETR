9ec9ab8a5e7e3a5dc6bdc4501d4222553ab2be81
luwelle0828 <luwelle0828@gmail.com>
2019-01-06 20:09:49 +0800

    Fb Login

--- src/pages/user/user-profile/user-profile.ts
+++ src/pages/user/user-profile/user-profile.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 import {LoginPage} from '../../login/login';
 import {UserEditPage} from '../user-edit/user-edit';
 import {UserSettingPage} from '../user-setting/user-setting';
-
+import firebase from 'firebase';
 /**
  * Generated class for the UserProfilePage page.
  *
@@ -18,20 +19,28 @@ import {UserSettingPage} from '../user-setting/user-setting';
 })
 export class UserProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: Facebook) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserProfilePage');
   }
-  goBack(){
-  	this.navCtrl.push( LoginPage );
-  }
   user_edit(){
-  	this.navCtrl.push(UserEditPage);
+  	this.navCtrl.setRoot(UserEditPage);
   }
   user_setting(){
-    this.navCtrl.push(UserSettingPage);
+    this.navCtrl.setRoot(UserSettingPage);
   }
+  facebookLogout(){
+    this.fb.logout().then( res => {
+      alert("Logged out.");
+      this.navCtrl.setRoot(LoginPage);
+      firebase.auth().signOut();
 
+      // CHECK LOGGED USER
+      // firebase.auth().onAuthStateChanged( fs =>{
+      //   console.log(fs);
+      // });
+    });
+  }
 }
