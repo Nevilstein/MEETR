1ab59ee8d541cd7ca30623da5e64eefcd860e53e
sopran123 <sfhsiono@gmail.com>
2019-01-11 02:10:51 +0800

    gmaps api and sdk applied

--- src/pages/user/user-geo/user-geo.ts
+++ src/pages/user/user-geo/user-geo.ts
@@ -1,38 +1,50 @@
-import { Component,EventEmitter  } from '@angular/core';
+import { Component,EventEmitter, ViewChild, ElementRef  } from '@angular/core';
 import { IonicPage, NavController, NavParams ,ModalController} from 'ionic-angular';
 import { DomSanitizer } from '@angular/platform-browser';
 import { AlertController } from 'ionic-angular';
 import { UserReportPage } from '../user-report/user-report';
 import { UserCheckPage } from '../user-check/user-check';
 import { Geolocation } from '@ionic-native/geolocation';
+import { google } from 'google-maps';
 
-/**
- * Generated class for the UserHomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+declare var google: any;
 
 @IonicPage()
 @Component({
   selector: 'page-user-geo',
   templateUrl: 'user-geo.html',
 })
+
 export class UserGeoPage {
 	lat:any;
 	lng:any;
+	@ViewChild('map') mapRef:ElementRef;
+	map:any;
 
 	constructor(private sanitizer: DomSanitizer, private alertCtrl: AlertController, private modal: ModalController, private geo:Geolocation) {
-
-
 	}
 
 	ionViewDidLoad(){
+		console.log(this.mapRef);
+		this.showMap();
+	}
+
+	showMap(){
+		//map location
 		this.geo.getCurrentPosition().then(pos=>{
 			this.lat = pos.coords.latitude;
 			this.lng = pos.coords.longitude;
 		}).catch(err => console.log(err));
+		//let location = new google.maps.LatLng(this.lat , this.lng);
+		let location = new google.maps.LatLng(14.6037159,120.9630088);
+		//map options
+		let options = {
+			center:location,
+			zoom:10
+		}
+		this.map = new google.maps.Map(this.mapRef.nativeElement,options);
 	}
+
 	onCardInteract(event){
    		console.log(event);
 	}
