5c86b80c81771ed58c42af665a5b791984f9d49e
Seth <sfhsiono@gmail.com>
2019-01-13 18:53:28 +0800

    map shows

--- src/pages/user/user-geo/user-geo.ts
+++ src/pages/user/user-geo/user-geo.ts
@@ -25,25 +25,27 @@ export class UserGeoPage {
 	}
 
 	ionViewDidLoad(){
-		console.log(this.mapRef);
 		this.showMap();
 	}
 
+	// JAVASCRIPT GMAPS ----works
 	showMap(){
 		//map location
 		this.geo.getCurrentPosition().then(pos=>{
 			this.lat = pos.coords.latitude;
 			this.lng = pos.coords.longitude;
 		}).catch(err => console.log(err));
-		let location = new google.maps.LatLng(this.lat , this.lng);
-		//let location = new google.maps.LatLng(14.6037159,120.9630088);
+		//let location = new google.maps.LatLng(this.lat , this.lng);
+		let location = new google.maps.LatLng(14.6037159,120.9630088);
 		//map options
 		let options = {
 			center:location,
-			zoom:10
+			zoom:15,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
 		}
 		this.map = new google.maps.Map(this.mapRef.nativeElement,options);
 	}
+	
 
 	onCardInteract(event){
    		console.log(event);
